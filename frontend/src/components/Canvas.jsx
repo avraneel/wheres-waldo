c4d5cd051ab02269980359f4438a5dd431022624
Avraneel Pal <avraneelpal22@gmail.com>
2026-09-13 17:41:48 +0530

    update

--- frontend/src/components/Canvas.jsx
+++ frontend/src/components/Canvas.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import imgUrl from "../assets/museum.png";
-import { boundingBoxLength } from "../globals";
 import styles from "../css/canvas.module.css";
 import { characters } from "../globals";
 
 export default function Canvas() {
   const canvasRef = useRef(null);
   const imgRef = useRef(null);
-  const popoverRef = useRef(null);
+  const [box, setBox] = useState({
+    x: -1,
+    y: -1,
+    locx: 0,
+    locy: 0,
+    show: false,
+  });
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -20,39 +25,66 @@ export default function Canvas() {
 
       for (let i = 0; i < canvas.width; i += 100) {
         for (let j = 0; j < canvas.height; j += 100) {
-          // TODO store this value somewhere
           ctx.strokeRect(i, j, 100, 100);
         }
       }
     });
 
-    canvas.addEventListener("click", (e) => {
-      const x = ~~(e.offsetX / 100);
-      const y = ~~(e.offsetY / 100);
-      console.log(x, y);
-      const popover = popoverRef.current;
-      popover.hidePopover();
-      popover.style.left = `${e.offsetX + boundingBoxLength / 2}px`;
-      popover.style.top = `${e.offsetY}px`;
-      popover.showPopover();
-    });
-
     img.src = imgUrl;
   }, []);
+
+  function handleClick(e) {
+    const canvas = e.currentTarget;
+    const container = canvas.parentElement;
+
+    const canvasRect = canvas.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect();
+
+    /**
+     * offsetY = offset from canvas, and top in css means top from container
+     * so we need to take into account the space between canvas and container
+     * so, offsetY + (canvas.top - container.top) gives that space
+     */
+
+    const locx = canvasRect.left - containerRect.left + e.nativeEvent.offsetX;
+    const locy = canvasRect.top - containerRect.top + e.nativeEvent.offsetY;
+
+    console.log(e.nativeEvent.offsetY);
+    setBox({
+      x: ~~(e.nativeEvent.offsetX / 100),
+      y: ~~(e.nativeEvent.offsetY / 100),
+      locx,
+      locy,
+      show: true,
+    });
+  }
 
   return (
     <div className={styles.canvasContainer}>
       <img ref={imgRef} src={imgUrl} alt="museum" className={styles.mapImage} />
-      <canvas ref={canvasRef} className={styles.mapCanvas}></canvas>
-      <div ref={popoverRef} className={styles.popoverNames} popover="auto">
-        <ContextMenu />
-      </div>{" "}
+      <canvas
+        ref={canvasRef}
+        className={styles.mapCanvas}
+        onClick={handleClick}
+      ></canvas>
+      {box.show && (
+        <div
+          className={styles.popoverNames}
+          style={{
+            top: box.locy,
+            left: box.locx,
+          }}
+        >
+          <ContextMenu x={box.x} y={box.y} />
+        </div>
+      )}
+      ;
     </div>
   );
 }
 
-function ContextMenu() {
-  const characterRef = useRef(null);
+function ContextMenu({ x, y }) {
+  console.log(x, y);
   const [chars, setChars] = useState(characters);
 
   function handleSubmit(e) {
@@ -67,13 +99,11 @@ function ContextMenu() {
       item.done === false && (
         <li key={index}>
           <form method="post" onSubmit={handleSubmit}>
-            <button ref={characterRef} value={item.name}>
-              {item.name}
-            </button>
+            <button value={item.name}>{item.name}</button>
           </form>
         </li>
       ),
   );
 
-  return <ul>{items}</ul>;
+  return <ul className={styles.charList}>{items}</ul>;
 }
